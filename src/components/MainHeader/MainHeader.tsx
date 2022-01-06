19f113bb99f682dd130fd15281fb29aaa142d7de
eli-zhang <zeliboid8@gmail.com>
2022-01-06 10:58:32 -0500

    Initial site header page

---
+++ src/components/MainHeader/MainHeader.tsx
@@ -0,0 +1,24 @@
+import { ImageContainer, Background, ShiftedTextContainer, Heading, Body, BodyElement, BoldText} from './MainHeaderStyled'
+
+const MainHeader = () => {
+    return (
+        <ImageContainer>
+            <Background src="/media/main_img.png" alt="background"/>
+            <ShiftedTextContainer>
+                <Heading>Hey, Im Eli.</Heading>
+                <Body>
+                    <BoldText>CS</BoldText> and <BoldText>ECE</BoldText> at Cornell
+                    <BodyElement pos={0}>Software Developer</BodyElement>
+                    <BodyElement pos={1}>Game Designer</BodyElement>
+                    <BodyElement pos={2}>Music Maker</BodyElement>
+                    <BodyElement pos={3}>Amateur Baker</BodyElement>
+                    <BodyElement pos={4}>Video Producer</BodyElement>
+                    <BodyElement pos={5}>Type Racer</BodyElement>
+                    <BodyElement pos={6}>Equate Lover</BodyElement>
+                </Body>
+            </ShiftedTextContainer>
+        </ImageContainer>
+    )
+}
+
+export default MainHeader;
